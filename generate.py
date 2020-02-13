7bd19f923c75f4b26b8defc2463eb57087e0e5dc
Chris <chris.darlington.music@gmail.com>
2020-02-13 22:44:06 +0000

    Update generate.py

--- generate.py
+++ generate.py
@@ -21,7 +21,7 @@ GENERATE_RES = 3
 IMAGE_SIZE = 224 # rows/cols
 IMAGE_CHANNELS = 3
 
-training_data = np.load('spell/guitars_data_small.npy')
+training_data = np.load('uploads/1llusion/guitars_data2.npy')
 
 def build_discriminator(image_shape):
     model = Sequential()
